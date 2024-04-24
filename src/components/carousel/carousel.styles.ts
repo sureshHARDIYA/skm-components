a7acba955ec296e8c6204d2cdc58434024e00e9d
Nhatduy209 <nhatduy20000@gmail.com>
2024-04-24 17:10:09 +0700

    Improvement carousel

--- src/components/carousel/carousel.styles.ts
+++ src/components/carousel/carousel.styles.ts
@@ -1,87 +1,120 @@
 import { css } from 'lit';
 
 export default css`
-  .body {
-    background: #3e3e3e;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
+
+  @keyframes fadeIn {
+    0% { opacity: 0.5; }
+    100% { opacity: 1; }
   }
 
-  .carousel {
-    width: 100vw;
-    height: 250px;
-    border-radius: 3px;
-    overflow: hidden;
+
+  .body {
+    background: #3e3e3e;
+  }
+
+  .container{
     position: relative;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-    &:hover .controls {
-      opacity: 1;
-    }
-
-    .controls {
-      opacity: 0;
-      display: flex;
-      position: absolute;
-      top: 50%;
-      left: 0;
-      justify-content: space-between;
-      width: 100%;
-      z-index: 99999;
-      transition: all ease 0.5s;
-
-      .control {
-        margin: 0 5px;
+    .carousel {
+      position: relative;
+      width: 100vw;
+      height: 250px;
+      border-radius: 3px;
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.2);
+  
+      &:hover .controls {
+        opacity: 1;
+      }
+  
+      .controls {
+        opacity: 0;
         display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 40px;
-        width: 40px;
-        border-radius: 50%;
-        background-color: rgba(255, 255, 255, 0.7);
-        opacity: 0.5;
-        transition: ease 0.3s;
-        cursor: pointer;
-
-        &:hover {
-          opacity: 1;
+        position: absolute;
+        top: 50%;
+        left: 0;
+        justify-content: space-between;
+        width: 100%;
+        transition: all ease 0.5s;
+  
+        .control {
+          margin: 0 5px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 40px;
+          width: 40px;
+          border-radius: 50%;
+          background-color: rgba(255, 255, 255, 0.7);
+          opacity: 0.5;
+          transition: ease 0.3s;
+          cursor: pointer;
+  
+          &:hover {
+            opacity: 1;
+          }
+        }
+      }
+  
+      .slides {
+        position: relative;
+        top: 50%;
+        left: 0;
+        transform: translateY(-50%);
+        display: flex;
+        width: 100%;
+  
+        > .slide {
+          min-width: 100%;
+          animation: fadeIn 2s;
+          position: relative;
+        }
+  
+        .text {
+          color: red;
+          z-index: 1000;
+          font-size: 15px;
+          padding: 8px 12px;
+          position: absolute;
+          bottom: 0px;
+          width: 100%;
+          text-align: center;
         }
       }
     }
-
-    .slides {
-      position: absolute;
-      top: 50%;
-      left: 0;
-      transform: translateY(-50%);
+  
+    .dots {
       display: flex;
-      width: 100%;
-      transition: 1s ease-in-out all;
-
-      > .slide {
-        min-width: 100%;
-        min-height: 250px;
-        height: auto;
+      justify-content: center;
+      position: absolute;
+      bottom: -20px;
+  
+      > .dot {
+        cursor: pointer;
+        background-color: #bbb;
+        margin-left : 5px;
+        height: 15px;
+        width: 15px;
+        border-radius: 50%;
+        border-radiu
+        flex-grow: 1;
+        margin-top: 10px;
+        transition: background-color 0.6s ease;
+  
+        &.active {
+          background: #717171;
+        }
+  
+        :hover {
+          background-color: #717171;
+        }
       }
     }
   }
 
-  .dots {
-    display: flex;
-    justify-content: center;
-    z-index: 1000000;
-    > .dot {
-      background: #fff;
-      height: 0.5vw;
-      flex-grow: 1;
-      margin-top: 10px;
-      &.active {
-        background: red;
-      }
-    }
-  }
+  
 
   .block {
     display: block;
@@ -90,4 +123,5 @@ export default css`
   .disappear {
     display: none;
   }
+
 `;
--- src/components/carousel/index.ts
+++ src/components/carousel/index.ts
@@ -4,28 +4,39 @@ import componentStyles from '../../styles/component.styles';
 
 import carouselStyle from './carousel.styles';
 
+type SlideProps = {
+  title: string;
+  url: string;
+};
+
 @customElement('skm-carousel')
 export default class SKMCarousel extends LitElement {
   static readonly styles = [componentStyles, carouselStyle];
 
   current = 0;
 
-  changeSlide = (next = true) => {
+  slideData: Array<SlideProps> = [
+    {
+      url: 'https://picsum.photos/1280/720?random=1',
+      title: 'SLide 1'
+    },
+    {
+      url: 'https://picsum.photos/1280/720?random=2',
+      title: 'SLide 2'
+    },
+    {
+      url: 'https://picsum.photos/1280/720?random=3',
+      title: 'SLide 3'
+    }
+  ];
+
+  checkActive = (currentSlide: number) => {
     const dots = this.shadowRoot?.querySelectorAll('.dots > .dot');
     const slides = this.shadowRoot?.querySelector('.slides');
     const slidesCount = this.shadowRoot?.querySelectorAll('.slides >.slide');
 
-    console.log('slide count', slides?.childElementCount);
-    console.log('current', this.current);
-
-    if (next) {
-      this.current = this.current > 1 ? 0 : this.current + 1;
-    } else {
-      this.current = this.current < 0 ? 0 : this.current - 1;
-    }
-
     for (let i = 0; i < (slides?.childElementCount ?? 3); i++) {
-      if (i !== this.current) {
+      if (i !== currentSlide) {
         dots?.[i].classList.remove('active');
         slidesCount?.[i]?.classList.remove('block');
         slidesCount?.[i]?.classList.add('disappear');
@@ -37,6 +48,16 @@ export default class SKMCarousel extends LitElement {
     }
   };
 
+  changeSlide = (next = true) => {
+    if (next) {
+      this.current = this.current > 1 ? 0 : this.current + 1;
+    } else {
+      this.current = this.current < 0 ? 0 : this.current - 1;
+    }
+
+    this.checkActive(this.current);
+  };
+
   firstUpdated() {
     // Controls
     this.shadowRoot?.querySelector('.next-slide')?.addEventListener('click', () => {
@@ -46,15 +67,30 @@ export default class SKMCarousel extends LitElement {
     this.shadowRoot?.querySelector('.prev-slide')?.addEventListener('click', () => {
       this.changeSlide(false);
     });
+
+    this.slideData.map((e, index) => {
+      this.shadowRoot
+        ?.getElementById(`slide_${index}`)
+        ?.addEventListener('click', () => this.checkActive(index));
+    });
   }
 
   render() {
-    return html`<div>
+    return html`<div class="container">
       <div class="carousel">
         <div class="slides">
-          <img src="https://picsum.photos/1280/720?random=1" alt="slide image" class="slide" />
-          <img src="https://picsum.photos/1280/720?random=2" alt="slide image" class="slide" />
-          <img src="https://picsum.photos/1280/720?random=3" alt="slide image" class="slide" />
+          ${this.slideData.map(
+            (slide) =>
+              html` <div class="slide">
+                <img
+                  src="${slide.url}"
+                  alt="slide image"
+                  class="slide"
+                  style="width: 100vw; height: 250px ; object-fit:cover" />
+                <div class="text">${slide.title}</div>
+                <div></div>
+              </div>`
+          )}
         </div>
         <div class="controls">
           <div class="control prev-slide">&#9668;</div>
@@ -63,9 +99,7 @@ export default class SKMCarousel extends LitElement {
       </div>
 
       <div class="dots">
-        <div class="dot active"></div>
-        <div class="dot"></div>
-        <div class="dot"></div>
+        ${this.slideData.map((dots, index) => html`<div class="dot" id="slide_${index}"></div>`)}
       </div>
     </div>`;
   }
