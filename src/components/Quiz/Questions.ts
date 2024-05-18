4317f40f4bc334c4addff9b2bb90fc6f4ed33ec7
Suresh KUMAR Mukhiya <itsmeskm99@gmail.com>
2024-05-18 10:10:37 +0200

    This is nice way to include question

--- src/components/Quiz/Questions.ts
+++ src/components/Quiz/Questions.ts
@@ -2,10 +2,10 @@ import { html, LitElement } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 
 import '@shoelace-style/shoelace/dist/components/badge/badge.js';
+import '@shoelace-style/shoelace/dist/components/radio/radio.js';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/checkbox/checkbox.js';
 import '@shoelace-style/shoelace/dist/components/textarea/textarea.js';
-import '@shoelace-style/shoelace/dist/components/radio/radio.js';
 import '@shoelace-style/shoelace/dist/components/radio-group/radio-group.js';
 
 import questionStyle from './questionStyle';
@@ -16,58 +16,62 @@ export class QuizQuestions extends LitElement {
 
   @property({ type: Array }) questions: any[] = [];
   @property({ type: Number }) totalQuestions = 0;
-  @state() currentQuestion = 0;
-  @state() selectedOptions: { questionIndex: number; optionId: string; isCorrect: boolean }[] = [];
+  @state() currentQuestionIndex = 0;
+  @state() selectedOptions: { [key: number]: { optionId: string; isCorrect: boolean } } = {};
   @state() response: { question: number; answers: string[]; text_answer?: string }[] = [];
 
   render() {
-    const isLastQuestion = this.currentQuestion === this.totalQuestions - 1;
-    const hasSelectedOption =
-      this.selectedOptions.some((option) => option.questionIndex === this.currentQuestion) ||
-      this.response.some((res) => res.question === this.currentQuestion);
-    return html`<div class="skm-quiz-outer-wrapper">
-      <sl-badge>${this.totalQuestions} questions</sl-badge>
-      <div class="question-wrapper">
-        <div class="question">${this.questions[this.currentQuestion].title}</div>
-        <div class="answers">${this.renderAnswerInput(this.questions[this.currentQuestion])}</div>
-        <div class="question-nav">
-          <sl-button
-            variant="default"
-            ?disabled=${this.currentQuestion === 0}
-            @click=${this.handlePrevious}
-            >Previous</sl-button
-          >
-          ${!isLastQuestion
-            ? html`<sl-button
-                variant="default"
-                ?disabled=${!hasSelectedOption}
-                @click=${this.handleNext}
-                >Next</sl-button
-              >`
-            : html``}
-          ${isLastQuestion
-            ? html`<sl-button variant="primary" @click=${this.handleSubmit}>Submit</sl-button>`
-            : html``}
+    const isLastQuestion = this.currentQuestionIndex === this.totalQuestions - 1;
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+
+    const hasSelectedOption = this.selectedOptions[this.currentQuestionIndex] !== undefined;
+
+    return html`
+      <div class="skm-quiz-outer-wrapper">
+        <sl-badge>${this.totalQuestions} questions</sl-badge>
+        <div class="question-wrapper">
+          <div class="question">${currentQuestion.title}</div>
+          <div class="answers">${this.renderAnswerInput()}</div>
+          <div class="question-nav">
+            <sl-button
+              variant="default"
+              ?disabled=${this.currentQuestionIndex === 0}
+              @click=${this.handlePrevious}
+              >Previous</sl-button
+            >
+            ${!isLastQuestion
+              ? html`<sl-button
+                  variant="default"
+                  ?disabled=${!hasSelectedOption}
+                  @click=${this.handleNext}
+                  >Next</sl-button
+                >`
+              : html``}
+            ${isLastQuestion
+              ? html`<sl-button variant="primary" @click=${this.handleSubmit}>Submit</sl-button>`
+              : html``}
+          </div>
         </div>
       </div>
-    </div>`;
+    `;
   }
 
-  renderAnswerInput(question: any) {
+  renderAnswerInput() {
+    const question = this.questions[this.currentQuestionIndex];
+    const selectedOption = this.selectedOptions[this.currentQuestionIndex]?.optionId || '';
+
     switch (question.technique) {
       case 0: // One choice
       case 3: // Survey
         return html`
-          <sl-radio-group name="answer" @sl-change=${(e: any) => this.handleOptionClick(e)}>
+          <sl-radio-group
+            id="answer-${this.currentQuestionIndex}"
+            name="answer-${this.currentQuestionIndex}"
+            @sl-change=${(e: any) => this.handleOptionClick(e)}
+            value=${selectedOption}>
             ${question.answer.map(
               (option: any) =>
-                html`
-                  <sl-radio
-                    value=${option.answer_text}
-                    ?checked=${this.isOptionSelected(question.id, option.answer_text)}>
-                    ${option.answer_text}
-                  </sl-radio>
-                `
+                html` <sl-radio value=${option.answer_text}> ${option.answer_text} </sl-radio> `
             )}
           </sl-radio-group>
         `;
@@ -77,9 +81,9 @@ export class QuizQuestions extends LitElement {
             (option: any) => html`
               <sl-checkbox
                 name="answer"
+                .checked=${this.isOptionSelected(option.answer_text)}
                 .value=${option.answer_text}
-                ?checked=${this.isOptionSelected(question.id, option.answer_text)}
-                @sl-change=${(e: any) => this.handleOptionClick(e, option.answer_text)}
+                @sl-change=${(e: any) => this.handleOptionClick(e)}
                 >${option.answer_text}</sl-checkbox
               >
             `
@@ -88,70 +92,76 @@ export class QuizQuestions extends LitElement {
       case 2: // Text answer
         return html`<sl-textarea
           name="answer"
-          @input=${(e: any) => this.handleFreeTextChange(e)}></sl-textarea>`;
+          @input=${(e: any) => this.handleFreeTextChange(e)}
+          value=${selectedOption}
+          placeholder="Enter your answer here"></sl-textarea>`;
       default:
         return html``;
     }
   }
 
-  isOptionSelected(questionId: number, optionId: string) {
-    console.log(
-      'selectedOptions:',
-      this.selectedOptions.some(
-        (option) => option.questionIndex === questionId && option.optionId === optionId
-      )
-    );
-
-    return this.selectedOptions.some(
-      (option) => option.questionIndex === questionId && option.optionId === optionId
-    );
+  isOptionSelected(optionId: string) {
+    return this.selectedOptions[this.currentQuestionIndex]?.optionId === optionId;
   }
 
   handleOptionClick(event: any) {
     const optionId = event.target.value;
-    const questionIndex = this.currentQuestion;
+    const questionIndex = this.currentQuestionIndex;
+
     const isCorrect = this.questions[questionIndex].answer.find(
       (answer: any) => answer.answer_text === optionId
     )?.is_correct_answer;
 
-    this.selectedOptions = [{ questionIndex, optionId, isCorrect }];
+    this.selectedOptions = {
+      ...this.selectedOptions,
+      [questionIndex]: { optionId, isCorrect }
+    };
   }
 
   handleFreeTextChange(event: any) {
     const value = event.target.value;
-    const questionIndex = this.currentQuestion;
-    this.selectedOptions = [{ questionIndex, optionId: value, isCorrect: true }];
+    const questionIndex = this.currentQuestionIndex;
+
+    this.selectedOptions = {
+      ...this.selectedOptions,
+      [questionIndex]: { optionId: value, isCorrect: true }
+    };
   }
 
   handlePrevious() {
-    if (this.currentQuestion > 0) {
-      this.currentQuestion -= 1;
-      this.selectedOptions = []; // Clear selected options when moving to previous question
+    if (this.currentQuestionIndex > 0) {
+      this.currentQuestionIndex -= 1;
     }
   }
 
   handleNext() {
-    if (this.currentQuestion < this.totalQuestions - 1) {
-      this.response.push({
-        question: this.currentQuestion,
-        answers: this.selectedOptions.map((option) => option.optionId),
-        text_answer: this.selectedOptions[0]?.optionId
-      });
-      this.currentQuestion += 1;
+    this.updateResponse();
+    if (this.currentQuestionIndex < this.totalQuestions - 1) {
+      this.currentQuestionIndex += 1;
     }
   }
 
   handleSubmit() {
-    this.response.push({
-      question: this.currentQuestion,
-      answers: this.selectedOptions.map((option) => option.optionId),
-      text_answer: this.selectedOptions[0]?.optionId
-    });
+    this.updateResponse();
     console.log('Response:', this.response);
     // Handle submission
   }
 
-  protected firstUpdated(): void {
-    console.log('firstUpdated', this.currentQuestion, this.questions[this.currentQuestion]);
+  updateResponse() {
+    const currentResponse = this.response.find((res) => res.question === this.currentQuestionIndex);
+
+    if (currentResponse) {
+      currentResponse.answers = [this.selectedOptions[this.currentQuestionIndex].optionId];
+      currentResponse.text_answer = this.selectedOptions[this.currentQuestionIndex]?.optionId;
+    } else {
+      this.response = [
+        ...this.response,
+        {
+          question: this.currentQuestionIndex,
+          answers: [this.selectedOptions[this.currentQuestionIndex].optionId],
+          text_answer: this.selectedOptions[this.currentQuestionIndex]?.optionId
+        }
+      ];
+    }
   }
 }
--- src/components/Quiz/questionStyle.ts
+++ src/components/Quiz/questionStyle.ts
@@ -2,6 +2,27 @@ import { css } from 'lit';
 
 export default css`
   .question-wrapper {
+    .question {
+      background-color: #f6f6f6;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      padding: 1rem;
+    }
+    .answers {
+      padding: 10px;
+      background: #f6f6f6;
+    }
+    sl-radio {
+      padding: 10px;
+
+      &:hover {
+        background: #f0f0f0;
+      }
+    }
+    sl-radio:not(:last-child) {
+      border-bottom: 1px solid #dadada;
+    }
+
     .question-nav {
       display: flex;
       justify-content: space-between;
