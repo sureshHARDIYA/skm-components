2c2ae97c14755c29a0acb4c4652d9f73919feda2
Suresh KUMAR Mukhiya <itsmeskm99@gmail.com>
2024-08-01 15:36:16 +0200

    Fix in prod

--- src/components/Quiz/constants.ts
+++ src/components/Quiz/constants.ts
@@ -1,3 +1,3 @@
-export const API_ROOT = 'https://dev.api.inspireu2iti.com/api/';
+export const API_ROOT = 'https://api.inspireu2iti.com/api/';
 export const username = 'itsmeskm99@gmail.com';
 export const password = 'Testing123#';
